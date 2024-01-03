7dd70792a3396865ace8e4faa0b1bbed399f9e13
Wyeth <contact.wyeth@gmail.com>
2024-01-03 12:02:00 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./assets/scss/index.scss";
 import App from "./App";
 import store from "./store";
@@ -9,10 +9,10 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
